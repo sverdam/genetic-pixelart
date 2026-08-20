f9d77f78eda5e8bb00f7e9887b12602120e460b7
KvotheGuile <kvothejoestar@gmail.com>
2026-08-20 13:31:38 -0700

    feat: basic drawing class

---
+++ src/algorithm/drawing.ts
@@ -0,0 +1,83 @@
+
+
+
+class Drawing{
+  private pixels: number[][];
+  public readonly rows: number;
+  public readonly cols: number;  
+  
+  constructor(rows: number, cols: number){
+    this.rows = rows;
+    this.cols = cols;
+
+    let randomColor = Math.round(Math.random() * 0xffffff);
+    this.pixels = Array.from({ length: rows }, () =>
+      Array(cols).fill(randomColor)
+    );
+  }
+
+  public Distance(original: Drawing):number {
+    let distance = 0;
+
+    this.pixels.forEach((row, rowIndex) => {
+      row.forEach((value, colIndex) => {
+        distance += Math.abs(value - original.get(rowIndex, colIndex));
+      });
+    });
+
+    return distance;
+  }
+
+  public Reproduce(mutation: number): Drawing {
+    
+    function toggleBit(num: number, position: number): number {
+    return num ^ (1 << position);
+    }
+    
+    const newDrawing = new Drawing(this.rows, this.cols);
+
+    this.pixels.forEach((row, rowIndex) => {
+      row.forEach((value, colIndex) => {
+        let val = value;
+        for (let i = 0; i < 24; i++)
+        {
+            if (Math.random() > mutation) continue;
+            val = toggleBit(val, i);
+        }
+        newDrawing.set(rowIndex, colIndex, value);
+      });
+    });
+
+    return newDrawing;
+  }
+
+  public set(row: number, col: number, value: number): void {
+    if (!this.pixels) return;
+    if (!this.pixels[row]) return;
+
+    if (this.isValidCoordinate(row, col)) {
+      this.pixels[row][col] = value;
+    } else {
+      throw new Error("Index out of bounds");
+    }
+  }
+
+  public get(row: number, col: number): number {
+    const DEFAULT_ERROR_RETURN = 0;
+    if (!this.pixels) return DEFAULT_ERROR_RETURN;
+    if (!this.pixels[row]) return DEFAULT_ERROR_RETURN;
+
+    if (this.isValidCoordinate(row, col)) {
+      return this.pixels[row][col] ? this.pixels[row][col] : DEFAULT_ERROR_RETURN;
+    }
+    throw new Error("Index out of bounds");
+  }
+
+  private isValidCoordinate(row: number, col: number): boolean {
+    return row >= 0 && row < this.rows && col >= 0 && col < this.cols;
+  }
+}
+
+
+
+
