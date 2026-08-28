e90bb4aa9516d4ce3c1efa93582da9ca84d6e19d
Sofía Verdugo <sverdugoam@gmail.com>
2026-08-27 19:45:16 -0700

    add canvas and display + handle functions

--- src/pages/DrawingPage.tsx
+++ src/pages/DrawingPage.tsx
@@ -1,22 +1,79 @@
-function DrawingPage() {
-  return <div>
-    <section>
-        <h1>SEE HOW THE ALGORITHM RECREATES THIS DRAWING!</h1>
-        <div className="card-container">
-            <div className="card">
-                <div className="canvas"></div>
-                <h2>DRAWING</h2>
-                <button className="btn">submit your drawing</button>
-            </div>
+import { useState } from "react";
 
-            <div className="card">
-                <div className="drawing"></div>
-                <h2>RESULT</h2>
-                <p>Here is the result of the algorithm trying to recreate the drawing.</p>
-            </div>
-        </div>
-    </section>
-  </div>;
+import Canvas from "../components/Canvas";
+import Display from "../components/Display";
+
+import Drawing from "../algorithm/drawing";
+import { geneticAlgorithm } from "../algorithm/evolution";
+
+export default function DrawingPage() {
+
+  const [generation, setGeneration] =
+    useState(0);
+
+  const [best, setBest] =
+    useState<Drawing | null>(null);
+
+  const [median, setMedian] =
+    useState<Drawing | null>(null);
+
+  const [worst, setWorst] =
+    useState<Drawing | null>(null);
+
+  async function handleDrawing(
+    drawing: Drawing
+  ) {
+
+    console.log(drawing.getPixelArray());
+
+    await geneticAlgorithm(
+      drawing,
+
+      1000,    // population size
+      0.001,  // mutation prob.
+      3000,    // generations
+      0.4,    // reproduction prob.
+      0.6,    // crossover prob.
+
+      (
+        generation,
+        best,
+        median,
+        worst
+      ) => {
+
+        setGeneration(generation);
+        setBest(best);
+        setMedian(median);
+        setWorst(worst);
+
+        console.log(
+          `Generation ${generation}`,
+          {
+            best: best.getScore(),
+            median: median.getScore(),
+            worst: worst.getScore(),
+          }
+        );
+      }
+    );
+  }
+
+  return (
+    <div className="pageGrid">
+
+      <Canvas
+        onSend={handleDrawing}
+        maxSize={500}
+      />
+
+      <Display
+        generation={generation}
+        best={best}
+        median={median}
+        worst={worst}
+      />
+
+    </div>
+  );
 }
-
-export default DrawingPage;
--- src/pages/ExamplePage.tsx
+++ src/pages/ExamplePage.tsx
@@ -1,22 +1,287 @@
-function ExamplePage() {
-  return <div>
-    <section>
-        <h1>SEE HOW THE ALGORITHM RECREATES THIS DRAWING!</h1>
-        <div className="card-container">
-            <div className="card">
-                <div className="drawing"></div>
-                <h2>DRAWING</h2>
-                <p>Here is the drawing that the algorithm will try to recreate.</p>
-            </div>
+import { useState } from "react";
 
-            <div className="card">
-                <div className="drawing"></div>
-                <h2>RESULT</h2>
-                <p>Here is the result of the algorithm trying to recreate the drawing.</p>
-            </div>
-        </div>
-    </section>
-  </div>;
+import ExampleDrawing from "../components/ExampleDrawing";
+import Display from "../components/Display";
+
+import Drawing from "../algorithm/drawing";
+import { geneticAlgorithm } from "../algorithm/evolution";
+
+const GRID_SIZE = 13;
+
+export default function ExamplePage() {
+
+  const [generation, setGeneration] =
+    useState(0);
+
+  const [best, setBest] =
+    useState<Drawing | null>(null);
+
+  const [median, setMedian] =
+    useState<Drawing | null>(null);
+
+  const [worst, setWorst] =
+    useState<Drawing | null>(null);
+
+
+  // squirrel drawing example
+  const exampleDrawing: number[][]=
+  [
+    [
+        8042439,
+        8042439,
+        8042439,
+        8042439,
+        8042439,
+        8042439,
+        8042439,
+        8042439,
+        8042439,
+        9947350,
+        9947350,
+        8042439,
+        8042439
+    ],
+    [
+        8042439,
+        9947350,
+        9947350,
+        16504748,
+        15910040,
+        8042439,
+        15910040,
+        16504748,
+        9947350,
+        9947350,
+        9947350,
+        9947350,
+        8042439
+    ],
+    [
+        9947350,
+        9947350,
+        12614234,
+        13207142,
+        13207142,
+        13207142,
+        13207142,
+        13207142,
+        12614234,
+        9947350,
+        9947350,
+        9947350,
+        8042439
+    ],
+    [
+        9947350,
+        12614234,
+        13207142,
+        13207142,
+        13207142,
+        12614234,
+        13207142,
+        13207142,
+        13207142,
+        12614234,
+        9947350,
+        10906188,
+        9947350
+    ],
+    [
+        9947350,
+        13207142,
+        15910040,
+        16504748,
+        13207142,
+        12614234,
+        13207142,
+        16504748,
+        15910040,
+        13207142,
+        10906188,
+        10906188,
+        10906188
+    ],
+    [
+        13207142,
+        16504748,
+        16504748,
+        5847594,
+        16504748,
+        13207142,
+        16504748,
+        5847594,
+        16504748,
+        16504748,
+        13207142,
+        10906188,
+        10906188
+    ],
+    [
+        16504748,
+        16504748,
+        16300963,
+        16099478,
+        16504748,
+        5847594,
+        16504748,
+        16099478,
+        16300963,
+        16504748,
+        16504748,
+        10906188,
+        9525309
+    ],
+    [
+        15910040,
+        16504748,
+        16504748,
+        16504748,
+        5847594,
+        5847594,
+        5847594,
+        16504748,
+        16504748,
+        16504748,
+        15910040,
+        9525309,
+        9525309
+    ],
+    [
+        8042439,
+        9947350,
+        16504748,
+        16504748,
+        16504748,
+        8736564,
+        16504748,
+        16504748,
+        16504748,
+        10906188,
+        9525309,
+        9525309,
+        9525309
+    ],
+    [
+        9947350,
+        9947350,
+        12614234,
+        16367774,
+        8736564,
+        9393721,
+        9393721,
+        16367774,
+        12614234,
+        8736564,
+        8736564,
+        9525309,
+        8736564
+    ],
+    [
+        9947350,
+        12614234,
+        13207142,
+        8736564,
+        10906188,
+        11693907,
+        11693907,
+        8736564,
+        13207142,
+        12614234,
+        8736564,
+        8736564,
+        8764781
+    ],
+    [
+        8764781,
+        13207142,
+        15910040,
+        16504748,
+        10906188,
+        10906188,
+        10906188,
+        16504748,
+        15910040,
+        13207142,
+        8736564,
+        9947350,
+        9947350
+    ],
+    [
+        8012841,
+        7552291,
+        6500633,
+        6894875,
+        6894875,
+        10906188,
+        6894875,
+        7552291,
+        8012841,
+        7552291,
+        6894875,
+        6894875,
+        6500633
+    ]
+];
+
+
+  async function handleDrawing(
+    drawing: Drawing
+  ) {
+
+    await geneticAlgorithm(
+      drawing,
+
+      1000,   // population size
+      0.001,  // mutation prob.
+      3000,   // generations
+      0.4,    // reproduction prob.
+      0.6,    // crossover prob.
+
+      (
+        generation,
+        best,
+        median,
+        worst
+      ) => {
+
+        setGeneration(generation);
+        setBest(best);
+        setMedian(median);
+        setWorst(worst);
+
+        console.log(
+          `Generation ${generation}`,
+          {
+            best: best.getScore(),
+            median: median.getScore(),
+            worst: worst.getScore(),
+          }
+        );
+      }
+    );
+  }
+
+
+  function handleExampleSend( drawing: Drawing) {
+    handleDrawing(drawing);
+  }
+
+  return (
+    <div className="pageGrid">
+
+      <ExampleDrawing
+        drawing={exampleDrawing}
+        onSend={handleExampleSend}
+        maxSize={500}
+      />
+
+      <Display
+        generation={generation}
+        best={best}
+        median={median}
+        worst={worst}
+      />
+
+    </div>
+  );
 }
-
-export default ExamplePage;
