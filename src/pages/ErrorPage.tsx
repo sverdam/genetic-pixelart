8230e84c881e9ad31c8fca0d0f43edb21b8f4252
Sofía Verdugo <sverdugoam@gmail.com>
2026-08-27 19:42:21 -0700

    change errorpage style

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,8 +1,13 @@
+import sadGuy from "../assets/sadGuy.gif"
+
+
 function ErrorPage() {
   return (
-    <div>
-      <h1>Error 404</h1>
+    <div className="col">
+      <h1 className="tiny5 xl">404</h1>
       <p>We couldnt find the page you were looking for :(</p>
+      <img src={sadGuy} alt="sorry :("/>
+      <a href="/" className="btn">Go home</a>
     </div>
   );
 }
